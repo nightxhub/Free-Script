44454ef5df363f572c60958a0fb959c10f8fc886
NightX <104092631+nightxhub@users.noreply.github.com>
2025-07-25 14:14:32 +0700

    Update Loader.lua

--- Prospecting/Loader.lua
+++ Prospecting/Loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/nightxhub/Free-Script/refs/heads/main/Anime%20Saga/Main.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/nightxhub/Free-Script/refs/heads/main/Prospecting/Main.lua"))()
