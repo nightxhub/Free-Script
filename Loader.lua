24870755d78d20feedee14264571ac5f83f5e174
NightX <104092631+nightxhub@users.noreply.github.com>
2025-08-12 21:51:58 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -3,7 +3,8 @@ local gameId = game.GameId
 local GameList = {
     [6765805766] = "Block%20Spin/Loader.lua",
     [6115988515] = "Anime%20Saga/Loader.lua",
-    [7629331599] = "Prospecting/Loader.lua"
+    [7629331599] = "Prospecting/Loader.lua",
+    [7326934954] = "99Nights/Loader.lua",
 }
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/nightxhub/Free-Script/main/" ..  GameList[gameId]))()
